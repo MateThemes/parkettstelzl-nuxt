d2f87a51b254eece1c47998d4e2911571a7f72d6
MateThemes <44877741+MateThemes@users.noreply.github.com>
2023-09-25 13:14:02 +0200

    Added scss file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,20 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ['@samk-dev/nuxt-uikit3'],
+  modules: [
+    '@samk-dev/nuxt-uikit3',
+    '@nuxtjs/google-fonts',
+  ],
   uikit3: {
+    css: {
+      coreCss: false,
+      coreTheme: false,
+      build: {
+        preprocessor: 'scss',
+        stylesPath: '~/assets/styles/app.scss', // our main scss entrypoint
+        //variablesPath: '~/assets/styles/variables.scss' // this is optional. set it if you want to access variables inside .vue files
+      }
+    },
     js: true,
     icons: true
   }
